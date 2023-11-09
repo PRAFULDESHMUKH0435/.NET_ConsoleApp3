06738c0e2c4eb5f4e2b38e181e12161ceb2a4f9d
PRAFUL <PRAFUL@DESKTOP-7GA0M9R>
2023-11-09 07:43:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            byte age = 21;
+            Console.WriteLine(age);
+            Console.ReadLine();
+        }
+    }
+}
