93e060b4bee0952cc3bf065fd62cbdf680012d2a
PRAFUL <PRAFUL@LAPTOP>
2023-11-10 07:17:31 +0530

    10 november

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,28 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            byte age = 21;
-            Console.WriteLine(age);
+            //Console.WriteLine("Hello World");
+            //byte age = 21;
+            //Console.WriteLine(age);
+            //Console.WriteLine("Hi");
+            //Console.ReadLine();
+            DataSet set = new DataSet();
+            DataTable table = new DataTable();
+            table.Columns.Add("Id");
+            table.Columns.Add("Name");
+            
+            for(int i = 0; i < 2; i++)
+            {
+                table.Rows.Add("1", "Praful");
+                //table.Rows.Add("2", "Kailash");
+            }
+
+
+            foreach(DataRow r in table.Rows)
+            {
+                Console.WriteLine(r["Id"] + " " + r["Name"]);
+                Console.WriteLine();
+            }
             Console.ReadLine();
         }
     }
